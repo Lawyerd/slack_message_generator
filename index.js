846b6193b427dea7b5e85866e68fe54723cf7218
이준석 <junseok@ijunseog-ui-MacBookPro.local>
2023-02-08 15:33:17 +0900

    Prototype

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { IncomingWebhook } = require('@slack/webhook');
+const Slack = require('slack-node');
 const MONGODB_URI = require('./db/mongoPassword').mongoPassword;
 
 // Connect to MongoDB
@@ -22,22 +22,45 @@ db.once('open', function() {
     instanceId: Number,
     Device: Object,
   });
-  const Data = mongoose.model("dvf-5000", deviceSchema);
 
-  // Watch the collection for changes
-  const changeStream = Data.watch();
-  changeStream.on('change', change => {
-    // If the state field has changed
-    if (change.operationType === 'insert') {
-      // Send a message to Slack'
-      const webhook = new IncomingWebhook('https://hooks.slack.com/services/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');
-      webhook.send({
-        text: `State changed to: ${change.fullDocument.state}`
-      }).then(res => {
-        console.log('Message sent: ', res.ts);
-      }).catch(err => {
-        console.error('Error: ', err);
-      });
-    }
+  const devices = [
+    {
+      name:"DVF-5000",
+      webhook: 'https://hooks.slack.com/services/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',
+      previousState:"INIT"
+    },
+    {
+      name:"DVF-6500",
+      webhook: 'https://hooks.slack.com/services/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',
+      previousState:"INIT"
+    },
+    {
+      name:"UNIT-35",
+      webhook: 'https://hooks.slack.com/services/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',
+      previousState:"INIT"
+    },
+  ]
+  
+  devices.forEach(device => {
+    const Data = mongoose.model(device.name, deviceSchema);
+
+    // Watch the collection for changes
+    const changeStream = Data.watch();
+    changeStream.on('change', function(change) {
+      // If the state field has changed
+      console.log('current State: '+change.fullDocument.state)
+      if (change.operationType === 'insert' && (device.previousState != change.fullDocument.state) && (device.previousState == 'RUNNING')) {
+        // Send a message to Slack
+        const slack = new Slack();
+        slack.setWebhook(device.webhook);
+        console.log(`previous State: ${device.previousState}, current State: ${change.fullDocument.state}`)
+        slack.webhook({
+          text: `State changed to: ${change.fullDocument.state} in collection ${device.name}`
+        }, function(err, response) {
+          // console.log(response);
+        });
+      }
+      device.previousState = change.fullDocument.state
+    });
   });
 });
