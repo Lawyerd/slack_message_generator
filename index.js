55f2744489f6163f54b52638fb53d8e5276ec5c9
Lawyerd <jun12376@gmail.com>
2023-03-09 11:43:01 +0900

    Add 'previousState checker'

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 const Slack = require('slack-node');
 const MONGODB_URI = require('./db/mongoPassword').mongoPassword;
+const { WebClient } = require('@slack/web-api')
+const token = 'xoxb-4790746604496-4752529706471-EhUhXbLErn3WCAPQJuSCICfw'
+const slackBot = new WebClient(token)
 
 // Connect to MongoDB
 mongoose.set('strictQuery', true);
@@ -23,47 +26,63 @@ db.once('open', function () {
     Device: Object,
   });
 
-  const devices = [
+  let devices = [
     {
       name: "DVF-5000",
       webhook: 'https://hooks.slack.com/services/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',
+      channel: 'C04ND0FHD3Q',
       previousState: "INIT"
     },
     {
       name: "DVF-6500",
       webhook: 'https://hooks.slack.com/services/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',
+      channel: 'C04NKJ77XFC',
       previousState: "INIT"
     },
     {
       name: "UNIT-35",
       webhook: 'https://hooks.slack.com/services/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',
+      channel: 'C04ND0GGXB8',
       previousState: "INIT"
     },
   ]
 
+  const sendMessage = async (message, channel) => {
+    try {
+      await slackBot.chat.postMessage({
+        channel: channel,
+        text: message
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+  let previousState = {};
+    for(let i = 0; i<devices.length; i++){
+      previousState[devices[i].name] = 'INIT'
+    }
+
+
+
   devices.forEach(device => {
     const Data = mongoose.model(device.name, deviceSchema);
+    
 
     // Watch the collection for changes
     const changeStream = Data.watch();
     changeStream.on('change', function (change) {
       if (change.operationType === 'insert') {
         // If the state field has changed
-        // console.log('current State: '+change.fullDocument.state)
+        // console.log(`previous State: ${device.previousState}, current State: ${change.fullDocument.state}`)
         if ((device.previousState != change.fullDocument.state) && (device.previousState == 'ACTIVE')) {
           // Send a message to Slack
-          const slack = new Slack();
-          slack.setWebhook(device.webhook);
-          console.log(`previous State: ${device.previousState}, current State: ${change.fullDocument.state}`)
-          slack.webhook({
-            text: `State changed to: ${change.fullDocument.state} in collection ${device.name}`
-          }, function (err, response) {
-            console.log(response.status);
-          });
+          console.log(`Send message to ${device.name} ${device.previousState} > ${change.fullDocument.state}`)
+          sendMessage(`State changed to: ${change.fullDocument.state} in collection ${device.name}`, device.channel)
         }
         device.previousState = change.fullDocument.state
-      }
 
-    });
-  });
-});
+      }
+    })
+  })
+})
---
+++ test.js
@@ -0,0 +1,19 @@
+const { WebClient } = require('@slack/web-api')
+const token = 'xoxb-4790746604496-4752529706471-EhUhXbLErn3WCAPQJuSCICfw'
+const slackBot = new WebClient(token)
+
+
+const sendMessage = async (message, channel) => {
+    try {
+      await slackBot.chat.postMessage({
+        channel: channel,
+        text: message
+      })
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
+
+
+sendMessage(`hi`, 'C04ND0FHD3Q')
